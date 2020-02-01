46c8fb2b140deca3b1be18e5ff5fc1eadec23aa8
Khoi Tran <khoitd@pascaliaasia.com>
2020-02-01 15:57:05 +0700

    add test database and env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBs7NHjVSaXRBbvi3TNMU9stejUOvvNrFs",
-    authDomain: "react-todo-udemy-01-2020.firebaseapp.com",
-    databaseURL: "https://react-todo-udemy-01-2020.firebaseio.com",
-    projectId: "react-todo-udemy-01-2020",
-    storageBucket: "react-todo-udemy-01-2020.appspot.com",
-    messagingSenderId: "292762406593",
-    appId: "1:292762406593:web:54bdb34f3cf100f85a0e40",
-    measurementId: "G-FJQRH5B6LZ"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -89,5 +89,3 @@ export {firebase, database};
 // database.ref().update({
 //     location: "new"
 // })
-
-
