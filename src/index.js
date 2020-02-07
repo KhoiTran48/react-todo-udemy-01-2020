544a10814e563a6ff4fe09b241e5ec09043a8999
Khoi Tran <khoitd@pascaliaasia.com>
2020-02-07 11:54:56 +0700

    add loading page

--- src/index.js
+++ src/index.js
@@ -8,19 +8,28 @@ import {login, logout} from './actions/auth'
 import {setTaskApi} from './actions/taskAction'
 
 console.log("test source map")
-ReactDOM.render(
-    <Provider store={store}>
-        <App/> 
-    </Provider>
-,document.getElementById("app"))
 
+const jsx = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+);
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         store.dispatch(login(user.uid))
         store.dispatch(setTaskApi()).then(()=>{
             console.log("set task api")
-
+            renderApp();
             // phải dùng chung object history được pass vào Router trong app.js
             // đó là lý do tại sao cần export history trong app.js
             history.push("/dashboard")
@@ -29,6 +38,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     } else {
         store.dispatch(logout())
         console.log("log out")
+        renderApp()
         history.push("/")
     }
 });
